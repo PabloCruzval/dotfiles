3f8de0223bed1b59b85678f4d175fbc56fc85e99
PabloCruzval <p.cruzat.valenzuela@gmail.com>
2025-12-20 17:22:48 -0300

    refactor(nvim): change global variables to locals

--- dot_config/nvim/lua/utils/terminal.lua
+++ dot_config/nvim/lua/utils/terminal.lua
@@ -1,24 +1,23 @@
-local split_win = function()
-	local actual_window = vim.api.nvim_get_current_win()
-	vim.cmd "split"
-	vim.api.nvim_set_current_win(actual_window)
-end
+local states = {
+	buff = nil,
+	win = nil
+}
 
 local SplitTerminal = function()
-	local buf_is_valid = vim.g.terminal_saved_buffer ~= nil and vim.api.nvim_buf_is_valid(vim.g.terminal_saved_buffer) or false
-	local win_is_valid = vim.g.terminal_saved_win ~= nil and vim.api.nvim_win_is_valid(vim.g.terminal_saved_win) or false
+	local buf_is_valid = states.buff ~= nil and vim.api.nvim_buf_is_valid(states.buff) or false
+	local win_is_valid = states.win ~= nil and vim.api.nvim_win_is_valid(states.win) or false
 	if win_is_valid and buf_is_valid then
-		vim.api.nvim_win_close(vim.g.terminal_saved_win, true)
-		vim.g.terminal_saved_win = nil
+		vim.api.nvim_win_close(states.win, true)
+		states.win = nil
 	elseif buf_is_valid then
-		split_win()
-		vim.api.nvim_set_current_buf(vim.g.terminal_saved_buffer)
-		vim.g.terminal_saved_win = vim.api.nvim_get_current_win()
+		vim.cmd "botright split"
+		vim.api.nvim_set_current_buf(states.buff)
+		states.win = vim.api.nvim_get_current_win()
 	else
-		split_win()
+		vim.cmd "botright split"
 		vim.cmd "terminal"
-		vim.g.terminal_saved_buffer = vim.api.nvim_get_current_buf()
-		vim.g.terminal_saved_win = vim.api.nvim_get_current_win()
+		states.buff = vim.api.nvim_get_current_buf()
+		states.win = vim.api.nvim_get_current_win()
 	end
 end
 
